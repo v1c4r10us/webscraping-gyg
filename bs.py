7930e3c715c816df089eba8ebdd9a420236e748e
v1c4r10us <edg.linkp@gmail.com>
2023-06-30 11:13:02 +0000

    adding orchestators

--- bs.py
+++ bs.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 import json
-<<<<<<< HEAD
 import sys
 
 def get_data(url):
@@ -70,34 +69,3 @@ def main():
 
 if __name__=="__main__":
     main()
-=======
-
-with open("sample.html") as fp:
-    soup = BeautifulSoup(fp, 'html.parser')
-
-#print(soup)
-data=[]
-for item in soup.find_all("li", class_="list-element"):
-    trip=dict()
-    picture=item.find("img")
-    trip['picture']=picture['src']
-    title=item.find("p", class_="vertical-activity-card__title")
-    trip['title']=title.text.strip() #title
-    typeof=item.find("span", class_="vertical-activity-card__activity-type c-classifier-badge")
-    trip['type']=typeof.text.strip() #type
-    list_features=[]
-    for feature in item.find_all("li", class_="activity-attributes__attribute"):
-        list_features.append(feature.text.strip())
-    trip['features']=list_features #features
-    data.append(trip)
-    rating=item.find("span", class_="rating-overall__rating-number rating-overall__rating-number--right")
-    trip['rating']=rating.text.strip() #rating
-    reviews=item.find("div", class_="rating-overall__reviews")
-    trip['reviews']=reviews.text.replace('(','').replace(')','').replace('reviews','').strip() #reviews
-    price=item.find("div", class_="baseline-pricing__value")
-    trip['price']=price.text.strip().replace("\xa0","").replace("\n","").replace(" ","").replace("From", "From ") #price
-    category_price=item.find("p", class_="baseline-pricing__category")
-    trip['category_price']=category_price.text.strip()
-
-print(data)
->>>>>>> 60ea5f1c9aebe687bf732a749b85f104ccc97ed1
